da1e74c921b7f50a309e1b003cd5c8bda70c3348
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-03-31 11:48:33 +0300

    CourseManagement

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import style from '../Header/style.module.scss';
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (<div className={style.wrapper}>
         <div className={style.item}>
             <h1>CoursesRUD.</h1>
             <div className={style.admin}>
-                <p className={style.headerAdmin}>Администрирование</p>
-                <p className={style.headerWitch}>Просмотр</p>
+                <p className={style.headerAdmin}><Link to={'/nav'}>Администрирование</Link></p>
+                <p className={style.headerWitch}><Link to={'/'}>Просмотр</Link></p>
             </div>
         </div>
     </div>);
